dcbaaa18c257fd1cf5717016df23e0ffa505ecca
Daniel Schroeder <dschroeder101@gmail.com>
2017-04-05 20:53:46 -0700

    Start work on universal routing

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -5,11 +5,6 @@ class Contact extends React.Component {
         return (
             <div className="content-area">
                 <h2>Contact</h2>
-                <br />
-                <p>Daniel Schroeder</p>
-                <p>19899 55A Avenue</p>
-                <p>Langley, BC</p>
-                <p><a href="mailto:dschroeder101@gmail.com">dschroeder101@gmail.com</a></p>
             </div>
         );
     }
