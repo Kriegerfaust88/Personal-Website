4bdc415e823d18fd0434046b34e029c127ffb908
Daniel Schroeder <dschroeder101@gmail.com>
2017-05-04 20:38:18 -0700

    Email form working in Dev

--- src/server.js
+++ src/server.js
@@ -1,37 +1,48 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 app.post('/send', function(req, res) {
-    const Email = require('emailjs/email');
-    var server = Email.server.connect({
-        host: 'localhost'
-    });
-    console.log(req.body);
-    server.send({
-        'text': req.body,
-        'from': req.body.email,
-        'to': 'dschroeder101@gmail.com',
-        'reply-to': req.body.email,
-        'subject': 'Sent from my personal site!'
-    }, function(error) {
-        if (error) {
-            return res.send({
-                status: 'failed'
-            });
-        } else {
-            return res.send({
-                status: 'OK'
-            });
-        }
-    })
+
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'dschroeder101@gmail.com',
+      pass: ***REMOVED***
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'dschroeder101@gmail.com',
+    subject: 'Personal Site - message from ' + name,
+    text: message
+  }
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error);
+      res.json({yo: 'error'});
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.json({yo: info.response});
+    };
+  })
 });
 
 const PORT = process.env.PORT || 8080;
